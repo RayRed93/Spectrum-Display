4e1f325d6650afb6063d57a30530f6608995a149
Piotr Redmerski <piotr.redmerski@gmail.com>
2017-05-25 19:30:23 +0200

    Add project files.

---
+++ Spectrum_Display/Spectrum_Display.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Spectrum_Display.ino
+ Created:	5/25/2017 7:30:13 PM
+ Author:	Piotr
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
